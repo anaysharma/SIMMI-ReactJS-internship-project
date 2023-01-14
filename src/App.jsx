08db07e541f7d0fbc917aec1f9dfb38c6d0db1bf
Anay Sharma <anaysharma8791@gmail.com>
2023-01-14 12:34:06 +0530

    finish App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,22 @@ import Profile from './pages/profilepage';
 function App() {
 	return (
 		<>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">HOME</Link>
+			<nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+				<ul className="container flex flex-wrap items-center justify-between mx-auto gap-2 max-w-4xl">
+					<li className="hover:text-blue-700 py-4 rounded-lg hover:bg-slate-100">
+						<Link className="py-4 px-8" to="/">
+							HOME
+						</Link>
 					</li>
-					<li>
-						<Link to="/about">ABOUT</Link>
+					<li className="mr-auto hover:text-blue-700 py-4 rounded-lg hover:bg-slate-100">
+						<Link className="py-4 px-8" to="/about">
+							ABOUT
+						</Link>
 					</li>
-					<li>
-						<Link to="/profile">PROFILE</Link>
+					<li className="hover:text-blue-700 py-4 rounded-lg hover:bg-slate-100">
+						<Link className="py-4 px-8" to="/profile">
+							PROFILE
+						</Link>
 					</li>
 				</ul>
 			</nav>
