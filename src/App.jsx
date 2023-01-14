87dfed5b4f4a92368326bc98e7703a8ef27540ae
Anay Sharma <anaysharma8791@gmail.com>
2023-01-14 11:31:36 +0530

    creat initial project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import About from './pages/aboutpage';
+import Home from './pages/homepage';
+import Profile from './pages/profilepage';
+
 function App() {
-  return <h1> Hello </h1>;
+	return (
+		<>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">HOME</Link>
+					</li>
+					<li>
+						<Link to="/about">ABOUT</Link>
+					</li>
+					<li>
+						<Link to="/profile">PROFILE</Link>
+					</li>
+				</ul>
+			</nav>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/profile" element={<Profile />} />
+			</Routes>
+		</>
+	);
 }
 
-export default App
+export default App;
